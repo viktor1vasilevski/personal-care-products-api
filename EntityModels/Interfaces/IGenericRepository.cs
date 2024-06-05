532fb467029f5f237a27d0d3865c4bb7ec6f8cf2
vik_vas <vasilevskiviktor@yahoo.com>
2024-06-05 19:26:22 +0200

    created interfaces for UoW and Generic Repo

---
+++ EntityModels/Interfaces/IGenericRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
+
+namespace EntityModels.Interfaces;
+
+public interface IGenericRepository<TEntity> where TEntity : class
+{
+    void Delete(object id);
+    void Delete(TEntity entity);
+    void DeleteRange(IEnumerable<TEntity> entities);
+    IEnumerable<TEntity> Get(
+        Expression<Func<TEntity, bool>> filter = null,
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+        Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null);
+    IQueryable<TEntity> GetAsQueryable(
+        Expression<Func<TEntity, bool>> filter = null,
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+        Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null);
+    TEntity GetByID(object id);
+    void Insert(TEntity entity);
+    void InsertRange(IEnumerable<TEntity> entities);
+    void Update(TEntity entity);
+    void UpdateWithRelatedEntities(TEntity entity);
+    object SetObjectStateToDetached(Object obj);
+    object SetObjectStateToAdded(object obj);
+    bool Exists(Expression<Func<TEntity, bool>> filter = null);
+    void DeleteRange(Expression<Func<TEntity, bool>> filter = null);
+}
---
+++ EntityModels/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using Core;
+
+namespace EntityModels.Interfaces;
+
+public interface IUnitOfWork<TContext> where TContext : IDbContext, new()
+{
+    IGenericRepository<TEntity> GetGenericRepository<TEntity>() where TEntity : class;
+    void Dispose();
+    void SaveChanges();
+    void RevertChanges();
+    void DetachAllEntities();
+    void Restart();
+    IDbContext ReturnContext();
+
+}
